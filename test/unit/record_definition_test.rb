224fe9c656b9c4fce2ae574510b3b4dc5210bb29
Wayne Robinson <wayne.robinson@gmail.com>
2011-03-23 10:05:51 +1000

    Test to make sure the  helper used in GenericApplication works correctly.

---
+++ test/unit/record_definition_test.rb
@@ -0,0 +1,27 @@
+require File.join(File.dirname(__FILE__), '../test_helper.rb')
+
+class RecordDefinitionTest < Test::Unit::TestCase
+  include TestHelper
+  
+  def setup
+    @client = Xeroizer::PublicApplication.new(CONSUMER_KEY, CONSUMER_SECRET)
+  end
+  
+  context "record definitions" do
+    
+    should "be defined correctly" do
+      [ 
+        :Account, :BrandingTheme, :Contact, :CreditNote, :Currency, :Invoice,
+        :Item, :Journal, :ManualJournal, :Organisation, :Payment, :TaxRate,
+        :TrackingCategory
+      ].each do | record_type |
+        record_factory = @client.send(record_type)
+        assert_kind_of(Xeroizer::Record::BaseModel, record_factory)
+        assert_kind_of(Xeroizer::GenericApplication, record_factory.application)
+        assert_equal(record_type.to_s, record_factory.model_name)
+      end
+    end
+    
+  end
+
+end
