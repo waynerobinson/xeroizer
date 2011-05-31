6cca763fb7b6c43454b682f78716c0175a27b493
Wayne Robinson <wayne.robinson@gmail.com>
2011-05-31 11:26:11 +1000

    Add additional fields to Organisation model.

--- lib/xeroizer/models/organisation.rb
+++ lib/xeroizer/models/organisation.rb
@@ -10,13 +10,19 @@ module Xeroizer
     
     class Organisation < Base
       
-      string :name
-      string :legal_name
-      boolean :pays_tax
-      string :version
-      string :organisation_type
-      string :base_currency
-      
+      string    :name
+      string    :legal_name
+      boolean   :pays_tax
+      string    :version
+      string    :organisation_type
+      string    :base_currency
+      integer   :financial_year_end_day
+      integer   :financial_year_end_month
+      date      :period_lock_date
+      date      :end_of_year_lock_date
+      string    :registration_number
+      datetime  :created_date_utc, :api_name => 'CreatedDateUTC'
+
     end
     
   end
