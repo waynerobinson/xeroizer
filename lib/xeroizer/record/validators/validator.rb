0351b47b4dab5536877d85feaa664fa87b103472
Isaac Kearse <isaac@kearse.co.nz>
2011-06-22 15:49:23 +1200

    Enable methods as validation conditions

--- lib/xeroizer/record/validators/validator.rb
+++ lib/xeroizer/record/validators/validator.rb
@@ -13,11 +13,17 @@ module Xeroizer
   
       def validate(record)
         run_validator = true
-        run_validator = false if options[:if] && !options[:if].call(record)
-        run_validator = false if options[:unless] && options[:unless].call(record)
+        run_validator = false if options[:if] && !condition?(record, options[:if])
+        run_validator = false if options[:unless] && condition?(record, options[:unless])
         valid?(record) if run_validator
       end
   
+      def condition?(record, condition)
+        return condition.call(record) if condition.respond_to? :call
+        return record.send(condition) if condition.is_a? Symbol
+        raise "Validation condition must be a Proc, a Symbol or a Module that responds to call"
+      end
+
     end
     
   end
