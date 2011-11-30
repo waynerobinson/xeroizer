2d2f9aded653ca96abef009402dedfda6f1209cf
Ben Biddington <ben.biddington@gmail.com>
2011-12-01 11:55:00 +1300

    Refactor: removed example test

---
+++ test/acceptance/about_private_applications.rb
@@ -0,0 +1,34 @@
+require File.expand_path File.join(File.dirname(__FILE__), "..", "test_helper")
+
+module Xeroizer
+  module Record
+    class BankTransactionModel < BaseModel
+      set_permissions :read
+    end
+
+    class BankTransaction < Base
+      string :type
+    end
+  end
+end
+
+Xeroizer::GenericApplication.class_eval do
+  record :BankTransaction
+end
+
+class AboutBankTransactions < Test::Unit::TestCase
+  def setup
+    assert_not_nil ENV["CONSUMER_KEY"], "No CONSUMER_KEY environment variable specified."
+    assert_not_nil ENV["CONSUMER_SECRET"], "No CONSUMER_SECRET environment variable specified."
+    assert_not_nil ENV["KEY_FILE"], "No KEY_FILE environment variable specified."
+    assert File.exists?(ENV["KEY_FILE"]), "The file <#{ENV["KEY_FILE"]}> does not exist."
+    @key_file = ENV["KEY_FILE"]
+    @consumer_key = ENV["CONSUMER_KEY"]
+    @consumer_secret = ENV["CONSUMER_SECRET"]
+  end
+
+  can "get all bank transactions" do
+    client = Xeroizer::PrivateApplication.new(@consumer_key, @consumer_secret, @key_file)
+    assert(client.BankTransaction.all.size > 0, "Expected at least one bank transaction")
+  end
+end
