3d2746e198546156387d478ba348787b44d14632
Ben Biddington <ben.biddington@gmail.com>
2011-12-01 14:24:35 +1300

    When parsing from XML, BankTransactionModel must include all of the root elements.

--- lib/xeroizer/models/bank_transaction.rb
+++ lib/xeroizer/models/bank_transaction.rb
@@ -7,6 +7,15 @@ module Xeroizer
     class BankTransaction < Base
       string :type
       date :date
+      string :line_amount_types
+      decimal :sub_total
+      decimal :total_tax
+      date :updated_date_utc
+      date :fully_paid_on_date
+      string :bank_transaction_id
+      boolean :is_reconciled
+
+      alias_method :reconciled?, :is_reconciled
     end
   end
 end
--- test/unit/models/bank_transaction_test.rb
+++ test/unit/models/bank_transaction_test.rb
@@ -27,7 +27,14 @@ class BankTransactionTest < Test::Unit::TestCase
 
     result = @instance.parse_response(some_xml)
 
-    assert_equal "SPEND", result.response_items.first.type
     assert_equal Date.parse("2010-07-30T00:00:00"), result.response_items.first.date
+    assert_equal "Inclusive", result.response_items.first.line_amount_types
+    assert_equal 15.00, result.response_items.first.sub_total
+    assert_equal 0.00, result.response_items.first.total_tax
+    assert_equal Date.parse("2008-02-20T12:19:56.657"), result.response_items.first.updated_date_utc
+    assert_equal Date.parse("2010-07-30T00:00:00"), result.response_items.first.fully_paid_on_date
+    assert_equal "d20b6c54-7f5d-4ce6-ab83-55f609719126", result.response_items.first.bank_transaction_id
+    assert_equal "SPEND", result.response_items.first.type
+    assert result.response_items.first.reconciled?, "Expected reconciled to be true"
   end
 end
