e0d322714e03cc0635d108b5ee3f58235f521467
Ben Biddington <ben.biddington@gmail.com>
2011-12-02 16:20:45 +1300

    Refactor: Don't need all of the xml payload to test contact parsing

--- test/unit/models/bank_transaction_model_parsing_test.rb
+++ test/unit/models/bank_transaction_model_parsing_test.rb
@@ -64,16 +64,6 @@ class BankTransactionModelParsingTest < Test::Unit::TestCase
               </Phones>
               <UpdatedDateUTC>2010-09-17T19:26:39.157</UpdatedDateUTC>
             </Contact>
-            <Date>2010-07-30T00:00:00</Date>
-            <LineAmountTypes>Inclusive</LineAmountTypes>
-            <SubTotal>15.00</SubTotal>
-            <TotalTax>0.00</TotalTax>
-            <Total>15.00</Total>
-            <UpdatedDateUTC>2008-02-20T12:19:56.657</UpdatedDateUTC>
-            <FullyPaidOnDate>2010-07-30T00:00:00</FullyPaidOnDate>
-            <BankTransactionID>d20b6c54-7f5d-4ce6-ab83-55f609719126</BankTransactionID>
-            <Type>SPEND</Type>
-            <IsReconciled>true</IsReconciled>
           </BankTransaction>
         </BankTransactions>
     </Response>"
