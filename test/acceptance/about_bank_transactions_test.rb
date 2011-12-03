b33541566ddb45b58632e05f6111b56a2abcef93
Ben Biddington <ben.biddington@gmail.com>
2011-12-03 13:47:47 +1300

    when requesting all bank transactions (i.e., without filter), it returns line items empty and contact with name and and id ONLY

--- test/acceptance/about_bank_transactions_test.rb
+++ test/acceptance/about_bank_transactions_test.rb
@@ -11,12 +11,23 @@ class AboutBankTransactions < Test::Unit::TestCase
     @consumer_secret = ENV["CONSUMER_SECRET"]
   end
   
-  can "get all bank transactions" do
-    client = Xeroizer::PrivateApplication.new(@consumer_key, @consumer_secret, @key_file)
-    assert(client.BankTransaction.all.size > 0, "Expected at least one bank transaction")
-  end
-  
-  context "given no filter is applied" do 
-    it "returns compact contact, and no line items at all"
+  context "when requesting all bank transactions (i.e., without filter)" do
+    setup do 
+      client = Xeroizer::PrivateApplication.new(@consumer_key, @consumer_secret, @key_file)
+      @the_bank_transactions = client.BankTransaction.all
+      @the_first_bank_transaction = @the_bank_transactions.first
+    end
+
+    it "returns line items empty" do 
+      assert_empty(@the_first_bank_transaction.line_items, "Expected line items to've been excluded")
+    end
+
+    it "returns contact with name and and id ONLY (no addresses or phones)" do 
+      the_contact = @the_first_bank_transaction.contact
+      assert_not_nil(the_contact.contact_id, "Expected contact id to be present")
+      assert_not_nil(the_contact.name, "Expected contact name to be present")
+      assert_empty the_contact.phones, "Expected empty contact phones"
+      assert_empty the_contact.addresses, "Expected empty contact addresses"
+    end
   end
 end
