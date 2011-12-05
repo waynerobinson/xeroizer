f61d1886a5a6071717ea577a2c336627ed21664a
Ben Biddington <ben.biddington@gmail.com>
2011-12-06 10:44:37 +1300

    BlockValidator uses a default message when validation fails and no message has been supplied

--- test/unit/record/block_validator_test.rb
+++ test/unit/record/block_validator_test.rb
@@ -63,4 +63,26 @@ class BlockValidatorTest < Test::Unit::TestCase
     assert_equal expected_error, record.errors.first[1],
       "There is an error, but it doesn't match"
   end
+
+  it "uses a default message when validation fails and no message has been supplied" do
+    record = @fake_record_class.new
+
+    the_block_returning_false = Proc.new{ false }
+
+    expected_error = "block condition failed"
+
+    block_validator = Xeroizer::Record::Validator::BlockValidator.new(
+      :name,
+      {
+        :block => the_block_returning_false
+      }
+    )
+
+    block_validator.valid?(record)
+
+    assert_equal 1, record.errors.size,
+      "Expected validation to fail with one error. #{record.errors.inspect}"
+    assert_equal expected_error, record.errors.first[1],
+      "There is an error, but it doesn't match"
+  end
 end
