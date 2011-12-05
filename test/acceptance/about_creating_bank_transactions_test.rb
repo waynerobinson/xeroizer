85091eafc55720d622f841fd2fe4afb2812194b7
Ben Biddington <ben.biddington@gmail.com>
2011-12-06 08:53:11 +1300

    [PASSING] can create bank transactions (provided you configure line items correctly)

--- test/acceptance/about_creating_bank_transactions_test.rb
+++ test/acceptance/about_creating_bank_transactions_test.rb
@@ -4,39 +4,38 @@ require "acceptance_test"
 class AboutCreatingBankTransactions < Test::Unit::TestCase
   include AcceptanceTest
 
-  must_eventually "create new bank transactions" do
+  can "create new bank transactions" do
     client = Xeroizer::PrivateApplication.new(@consumer_key, @consumer_secret, @key_file)
 
-    account = client.Account.all.first
+    account = client.Account.all.select{|account| account.status == "ACTIVE" && account.type == "REVENUE"}.first
+    bank_account = client.Account.all.select{|account| account.status == "ACTIVE" && account.type == "BANK"}.first
 
     new_transaction = client.BankTransaction.build(
-      :type => "SPEND", 
+      :type => "SPEND",
       :contact => { :name => "Jazz Kang" },
-      :account_code => account.code,
-      :tax_type => "XXX", 
       :line_items => [
-        :item_code => "xxx", 
-        :description => "description",
-        :quantity => 1.0,
-        :unit_amount => 3.99
-      ], 
-      :bank_account => { :code => account.code }
+        :item_code => "Clingfilm bike shorts",
+        :description => "Bike shorts made of clear, unbreathable material",
+        :quantity => 1,
+        :unit_amount => 39.99,
+        :account_code => account.code,
+        :tax_type => account.tax_type
+      ],
+      :bank_account => { :code => bank_account.code }
     )
-    
-    assert new_transaction.save, "Save failed with the following errors: #{new_transaction.errors.inspect}"
 
-    puts new_transaction.inspect
+    assert new_transaction.save, "Save failed with the following errors: #{new_transaction.errors.inspect}"
   end
 
-  it "fails with RuntimeError when you try and create a new bank account" do 
+  it "fails with RuntimeError when you try and create a new bank account" do
     client = Xeroizer::PrivateApplication.new(@consumer_key, @consumer_secret, @key_file)
-    
+
     new_account = client.Account.build(
-      :name => "Example bank account",  
-      :code => "ACC-001"                                         
+      :name => "Example bank account",
+      :code => "ACC-001"
     )
-    
-    assert_raise RuntimeError do 
+
+    assert_raise RuntimeError do
       new_account.save
     end
   end
