5267f59d599485bdc4d65571b7027116ef94151d
Ben Biddington <ben.biddington@gmail.com>
2011-12-06 16:48:33 +1300

    Introduced new file for tests describing BankTransaction calculation behaviour

--- test/unit/models/bank_account_parsing_test.rb
+++ test/unit/models/bank_account_parsing_test.rb
@@ -34,37 +34,4 @@ class BankAccountParsingTest < Test::Unit::TestCase
     assert_equal "BANK", the_bank_account.code, 
       "Unexpected code: #{the_bank_account}"
   end
-
-  include Xeroizer::Record
-
-  must "calculate the total as the sum of the line items correctly (including tax)" do 
-    the_line_items = [
-      {
-        :quantity => 1,
-        :unit_amount => 39.99,
-        :tax_amount => 2.99
-      },
-      {
-        :quantity => 1,
-        :unit_amount => 9.99,
-        :tax_amount => 0.99
-      }
-    ]
-
-    fake_parent = Class.new do
-      attr_accessor :application
-    end.new
-
-    the_bank_transaction = BankTransaction.new fake_parent
-    the_bank_transaction.line_items = the_line_items
-
-    expected_total = the_line_items.map do |line_item|
-      item_total = line_item[:unit_amount].to_f + line_item[:tax_amount].to_f
-      (item_total * line_item[:quantity].to_f).to_f
-    end.reduce :+
-
-    assert_equal expected_total, the_bank_transaction.total
-  end
-
-  must "calculate the total tax as expected (based solely on the tax type)"
 end
