e70721ed427b048fcd2b9dde6a07145ceb5026db
Ben Biddington <ben.biddington@gmail.com>
2011-12-06 13:55:25 +1300

    Refactor: removed tests that're better left as units.

--- test/acceptance/about_creating_bank_transactions_test.rb
+++ test/acceptance/about_creating_bank_transactions_test.rb
@@ -41,32 +41,4 @@ class AboutCreatingBankTransactions < Test::Unit::TestCase
       new_account.save
     end
   end
-
-  must "supply either SPEND or RECEIVE as the type" do
-    all_accounts = client.Account.all
-
-    account = all_accounts.select{|account| account.status == "ACTIVE" && account.type == "REVENUE"}.first
-    bank_account = all_accounts.select{|account| account.status == "ACTIVE" && account.type == "BANK"}.first
-
-    new_transaction = client.BankTransaction.build(
-      :type => "ANYTHING_BUT_SPEND_OR_RECEIVE",
-      :contact => { :name => "Jazz Kang" },
-      :line_items => [
-        :item_code => "Clingfilm bike shorts",
-        :description => "Bike shorts made of clear, unbreathable material",
-        :quantity => 1,
-        :unit_amount => 39.99,
-        :account_code => account.code,
-        :tax_type => account.tax_type
-      ],
-      :bank_account => { :code => bank_account.code }
-    )
-
-    assert false == new_transaction.save, "Expected save to fail"
-    assert_equal "Invalid type. Expected either SPEND or RECEIVE.", new_transaction.errors_for(:type).first
-  end
-
-  must "supply supply a contact"
-  must "supply one or more line items"
-  must "supply a bank account"
 end
