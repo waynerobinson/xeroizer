8b157408ab362d55e829eeca3f94704a8f2d2ad1
Ben Biddington <ben.biddington@gmail.com>
2011-12-09 16:53:13 +1300

    Refactor: include, plus expanded line_item count assertion

--- test/unit/models/bank_transaction_validation_test.rb
+++ test/unit/models/bank_transaction_validation_test.rb
@@ -1,8 +1,10 @@
 require "test_helper"
 
 class BankTransactionValidationTest < Test::Unit::TestCase
+  include Xeroizer::Record
+
   must "supply either SPEND or RECEIVE as the type" do
-    instance = Xeroizer::Record::BankTransaction.build({:type => "xxx"}, nil)
+    instance = BankTransaction.build({:type => "xxx"}, nil)
     
     assert false == instance.valid?, "Expected invalid because of invalid type"
     
@@ -10,13 +12,13 @@ class BankTransactionValidationTest < Test::Unit::TestCase
 
     assert_equal expected_error, instance.errors_for(:type).first, "Expected an error about type"
 
-    instance = Xeroizer::Record::BankTransaction.build({:type => "SPEND"}, nil)
+    instance = BankTransaction.build({:type => "SPEND"}, nil)
     
     instance.valid?
     
     assert_empty instance.errors_for(:type), "Expected no error about type"
 
-    instance = Xeroizer::Record::BankTransaction.build({:type => "RECEIVE"}, nil)
+    instance = BankTransaction.build({:type => "RECEIVE"}, nil)
     
     instance.valid?
     
@@ -24,7 +26,7 @@ class BankTransactionValidationTest < Test::Unit::TestCase
   end
 
   must "supply a non-blank contact" do
-    instance = Xeroizer::Record::BankTransaction.build({}, nil)
+    instance = BankTransaction.build({}, nil)
     
     assert false == instance.valid?, "Expected invalid because of missing contact"
 
@@ -34,18 +36,32 @@ class BankTransactionValidationTest < Test::Unit::TestCase
 
   must "supply at least one line item" do
     zero_line_items = []
-    
-    instance = Xeroizer::Record::BankTransaction.build({:line_items => zero_line_items}, nil)
-    
+
+    instance = BankTransaction.build({:line_items => zero_line_items}, nil)
+
     assert false == instance.valid?, "Expected invalid because of empty line items"
 
-    assert_equal "Invalid line items. Must supply at least one.", instance.errors_for(:line_items).first, 
+    assert_equal "Invalid line items. Must supply at least one.", instance.errors_for(:line_items).first,
       "Expected an error about blank line items"
+
+    one_line_item = [LineItem.build({}, nil)]
+
+    instance.errors.clear
+    instance.line_items = one_line_item
+
+    assert_empty instance.errors_for(:line_items), "expected no errors for one line item, #{instance.errors_for(:line_items)}"
+
+    more_than_one_line_item = [LineItem.build({}, nil), LineItem.build({}, nil)]
+
+    instance.errors.clear
+    instance.line_items = more_than_one_line_item
+
+    assert_empty instance.errors_for(:line_items), "expected no errors for more than one line item, #{instance.errors_for(:line_items)}"
   end
 
   must "supply a non-blank bank account" do
-    instance = Xeroizer::Record::BankTransaction.build({}, nil)
-    
+    instance = BankTransaction.build({}, nil)
+
     assert false == instance.valid?, "Expected invalid because of missing bank account"
 
     assert_equal "can't be blank", instance.errors_for(:bank_account).first, 
@@ -53,7 +69,7 @@ class BankTransactionValidationTest < Test::Unit::TestCase
   end
 
   must "supply valid line_amount_types value" do 
-    instance = Xeroizer::Record::BankTransaction.build({
+    instance = BankTransaction.build({
       :line_amount_types => "XXX_ANYTHING_INVALID_XXX"
     }, nil)
 
@@ -64,7 +80,7 @@ class BankTransactionValidationTest < Test::Unit::TestCase
   end
 
   must "line_amount_type defaults to \"Exclusive\"" do
-    instance = Xeroizer::Record::BankTransaction.build({}, nil)
+    instance = BankTransaction.build({}, nil)
 
     assert_equal "Exclusive", instance.line_amount_types
   end
