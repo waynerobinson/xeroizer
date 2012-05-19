58a05dee117845f39131b636c14e0381d3e9dc11
Adam Cooke <adam@atechmedia.com>
2012-05-19 19:03:08 +0100

    send all create requests using a POST request (untested, could break, ymmv etc...)

--- lib/xeroizer/record/base.rb
+++ lib/xeroizer/record/base.rb
@@ -131,7 +131,7 @@ module Xeroizer
           request = to_xml
           log "[CREATE SENT] (#{__FILE__}:#{__LINE__}) #{request}"
           
-          response = parent.http_put(request)
+          response = parent.http_post(request)
           log "[CREATE RECEIVED] (#{__FILE__}:#{__LINE__}) #{response}"
           
           parse_save_response(response)
