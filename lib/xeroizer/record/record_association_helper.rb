807708a9415e925d6067bbdd6b681ae6283ae260
Wayne Robinson <wayne.robinson@gmail.com>
2013-04-10 13:54:54 +1000

    Allow Base Module Name to be Passed to Helpers

--- lib/xeroizer/record/record_association_helper.rb
+++ lib/xeroizer/record/record_association_helper.rb
@@ -10,8 +10,7 @@ module Xeroizer
 
         def belongs_to(field_name, options = {})
           internal_field_name = options[:internal_name] || field_name
-          internal_singular_field_name = internal_field_name.to_s.singularize
-
+          internal_singular_field_name = options[:internal_name_singular] || internal_field_name.to_s.singularize
           define_association_attribute(field_name, internal_singular_field_name, :belongs_to, options)
 
           # Create a #build_record_name method to build the record.
@@ -25,15 +24,14 @@ module Xeroizer
             model_parent = new_model_class(model_name)
 
             # Create a new record, binding it to it's parent instance.
-            record = Xeroizer::Record.const_get(model_name).build(attributes, model_parent)
+            record = (options[:base_module] || Xeroizer::Record).const_get(model_name).build(attributes, model_parent)
             self.attributes[field_name] = record
           end
         end
 
         def has_many(field_name, options = {})
           internal_field_name = options[:internal_name] || field_name
-          internal_singular_field_name = internal_field_name.to_s.singularize
-
+          internal_singular_field_name = options[:internal_name_singular] || internal_field_name.to_s.singularize
           define_association_attribute(field_name, internal_field_name, :has_many, options)
 
           # Create an #add_record_name method to build the record and add to the attributes.
@@ -45,7 +43,7 @@ module Xeroizer
             model_parent = new_model_class(model_name)
 
             # The class of this record.
-            record_class = Xeroizer::Record.const_get(model_name)
+            record_class = (options[:base_module] || Xeroizer::Record).const_get(model_name)
 
             # Parse the *args variable so that we can use this method like:
             #   add_record({fields}, {fields}, ...)
@@ -83,11 +81,10 @@ module Xeroizer
           define_simple_attribute(field_name, association_type, options.merge!(:skip_writer => true), ((association_type == :has_many) ? [] : nil))
 
           internal_field_name = options[:internal_name] || field_name
-          internal_singular_field_name = internal_field_name.to_s.singularize
+          internal_singular_field_name = options[:internal_name_singular] || internal_field_name.to_s.singularize
           model_name = options[:model_name] ? options[:model_name].to_sym : field_name.to_s.singularize.camelize.to_sym
-
           define_method "#{internal_field_name}=".to_sym do | value |
-            record_class = Xeroizer::Record.const_get(model_name)
+            record_class = (options[:base_module] || Xeroizer::Record).const_get(model_name)
             case value
               when Hash
                 self.attributes[field_name] = ((association_type == :has_many) ? [] : nil)
@@ -95,10 +92,8 @@ module Xeroizer
                   when :has_many
                     self.attributes[field_name] = []
                     self.send("add_#{internal_singular_field_name}".to_sym, value)
-
                   when :belongs_to
-                    self.attributes[field_name] = Xeroizer::Record.const_get(model_name).build(value, new_model_class(model_name))
-
+                    self.attributes[field_name] = (options[:base_module] || Xeroizer::Record).const_get(model_name).build(value, new_model_class(model_name))
                 end
 
               when Array
--- lib/xeroizer/record/xml_helper.rb
+++ lib/xeroizer/record/xml_helper.rb
@@ -12,7 +12,7 @@ module Xeroizer
       module ClassMethods
         
         # Build a record instance from the XML node.
-        def build_from_node(node, parent)
+        def build_from_node(node, parent, base_module)
           record = new(parent)
           node.elements.each do | element |
             field = self.fields[element.name.to_s.underscore.to_sym]
@@ -25,17 +25,17 @@ module Xeroizer
                 when :decimal     then BigDecimal.new(element.text)
                 when :date        then Date.parse(element.text)
                 when :datetime    then Time.parse(element.text)
-                when :datetime_utc then ActiveSupport::TimeZone['UTC'].parse(element.text).utc.round(3)
+                when :datetime_utc then ActiveSupport::TimeZone['UTC'].parse(element.text).utc
                 when :belongs_to  
                   model_name = field[:model_name] ? field[:model_name].to_sym : element.name.to_sym
-                  Xeroizer::Record.const_get(model_name).build_from_node(element, parent)
+                  base_module.const_get(model_name).build_from_node(element, parent, base_module)
                   
                 when :has_many
                   if element.element_children.size > 0
                     sub_field_name = field[:model_name] ? field[:model_name].to_sym : element.children.first.name.to_sym
                     sub_parent = record.new_model_class(sub_field_name)
                     element.children.inject([]) do | list, element |
-                      list << Xeroizer::Record.const_get(sub_field_name).build_from_node(element, sub_parent)
+                      list << base_module.const_get(sub_field_name).build_from_node(element, sub_parent, base_module)
                     end
                   end
 
