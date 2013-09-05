6ad7be4364e405a423cfc5d8d952cd4c273724bf
Oxana Goutnik <og@syrec.org>
2013-09-04 19:51:01 -0500

    attempt to fix payroll

--- lib/xeroizer/record/base_model_http_proxy.rb
+++ lib/xeroizer/record/base_model_http_proxy.rb
@@ -108,6 +108,7 @@ module Xeroizer
               when :datetime    then [field[:api_name], expression, "DateTime.Parse(\"#{value.utc.strftime("%Y-%m-%dT%H:%M:%S")}\")"]
               when :belongs_to  then
               when :has_many    then
+              when :has_one    then
             end
           end
 
