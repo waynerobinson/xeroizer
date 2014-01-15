2a16112e17615520a5eef2e905f982448b01911e
Wayne Robinson <wayne.robinson@gmail.com>
2014-01-16 09:24:46 +1100

    Add Contact People Association

--- lib/xeroizer/models/contact.rb
+++ lib/xeroizer/models/contact.rb
@@ -1,23 +1,25 @@
+require "xeroizer/models/contact_person"
+
 module Xeroizer
   module Record
-    
+
     class ContactModel < BaseModel
-            
+
       set_permissions :read, :write, :update
-      
+
     end
-    
+
     class Contact < Base
-      
+
       CONTACT_STATUS = {
         'ACTIVE' =>     'Active',
         'DELETED' =>    'Deleted'
       } unless defined?(CONTACT_STATUS)
-      
+
       set_primary_key :contact_id
       set_possible_primary_keys :contact_id, :contact_number
       list_contains_summary_only true
-      
+
       guid          :contact_id
       string        :contact_number
       string        :contact_status
@@ -35,15 +37,20 @@ module Xeroizer
       datetime_utc  :updated_date_utc, :api_name => 'UpdatedDateUTC'
       boolean       :is_supplier
       boolean       :is_customer
-      
+
       has_many  :addresses, :list_complete => true
       has_many  :phones, :list_complete => true
       has_many  :contact_groups
-      
+      has_many  :contact_persons, :internal_name => :contact_people
+
       validates_presence_of :name
       validates_inclusion_of :contact_status, :in => CONTACT_STATUS.keys, :allow_blanks => true
-      
+
+      def email_address?
+        email_address.present?
+      end
+
     end
-    
+
   end
 end
--- lib/xeroizer/record/record_association_helper.rb
+++ lib/xeroizer/record/record_association_helper.rb
@@ -1,41 +1,41 @@
 module Xeroizer
   module Record
     module RecordAssociationHelper
-      
+
       def self.included(base)
-        base.extend(ClassMethods)    
+        base.extend(ClassMethods)
       end
-      
+
       module ClassMethods
-        
+
         def belongs_to(field_name, options = {})
           internal_field_name = options[:internal_name] || field_name
           internal_singular_field_name = internal_field_name.to_s.singularize
-          
+
           define_association_attribute(field_name, internal_singular_field_name, :belongs_to, options)
-        
+
           # Create a #build_record_name method to build the record.
           define_method "build_#{internal_singular_field_name}" do | *args |
             attributes = args.size == 1 ? args.first : {}
-          
+
             # The name of the record model.
             model_name = options[:model_name] ? options[:model_name].to_sym : field_name.to_s.singularize.camelize.to_sym
-          
+
             # The record's parent instance for this current application.
             model_parent = new_model_class(model_name)
-          
+
             # Create a new record, binding it to it's parent instance.
             record = Xeroizer::Record.const_get(model_name).build(attributes, model_parent)
             self.attributes[field_name] = record
           end
         end
-      
+
         def has_many(field_name, options = {})
           internal_field_name = options[:internal_name] || field_name
           internal_singular_field_name = internal_field_name.to_s.singularize
-        
+
           define_association_attribute(field_name, internal_field_name, :has_many, options)
-                  
+
           # Create an #add_record_name method to build the record and add to the attributes.
           define_method "add_#{internal_singular_field_name}" do | *args |
             # The name of the record model.
@@ -43,7 +43,7 @@ module Xeroizer
 
             # The record's parent instance for this current application.
             model_parent = new_model_class(model_name)
-          
+
             # The class of this record.
             record_class = Xeroizer::Record.const_get(model_name)
 
@@ -55,15 +55,15 @@ module Xeroizer
             records = []
             if args.size == 1 && args.first.is_a?(Array)
               records = args.first
-            elsif args.size > 0 
+            elsif args.size > 0
               records = args
             else
               raise StandardError.new("Invalid arguments for #{self.class.name}#add_#{internal_singular_field_name}(#{args.inspect}).")
             end
-          
+
             # Ensure that complete record is downloaded before adding new records
-            self.send(field_name)
-            
+            self.send(internal_field_name)
+
             # Add each record.
             last_record = nil
             records.each do | record |
@@ -73,34 +73,34 @@ module Xeroizer
               self.attributes[field_name] << record
               last_record = record
             end
-            
+
             last_record # last record
           end
-          
+
         end
-        
+
         def define_association_attribute(field_name, internal_field_name, association_type, options)
           define_simple_attribute(field_name, association_type, options.merge!(:skip_writer => true), ((association_type == :has_many) ? [] : nil))
-          
+
           internal_field_name = options[:internal_name] || field_name
           internal_singular_field_name = internal_field_name.to_s.singularize
           model_name = options[:model_name] ? options[:model_name].to_sym : field_name.to_s.singularize.camelize.to_sym
-    
-          define_method "#{internal_field_name}=".to_sym do | value | 
+
+          define_method "#{internal_field_name}=".to_sym do | value |
             record_class = Xeroizer::Record.const_get(model_name)
             case value
-              when Hash 
+              when Hash
                 self.attributes[field_name] = ((association_type == :has_many) ? [] : nil)
                 case association_type
-                  when :has_many  
+                  when :has_many
                     self.attributes[field_name] = []
                     self.send("add_#{internal_singular_field_name}".to_sym, value)
-                    
-                  when :belongs_to  
+
+                  when :belongs_to
                     self.attributes[field_name] = Xeroizer::Record.const_get(model_name).build(value, new_model_class(model_name))
-                    
+
                 end
-                
+
               when Array
                 self.attributes[field_name] = ((association_type == :has_many) ? [] : nil)
                 value.each do | single_value |
@@ -110,18 +110,18 @@ module Xeroizer
                     else                   raise AssociationTypeMismatch.new(record_class, single_value.class)
                   end
                 end
-                
+
               when record_class
                 self.attributes[field_name] = ((association_type == :has_many) ? [value] : value)
-                
+
               when NilClass
                 self.attributes[field_name] = []
-                
+
               else
                 raise AssociationTypeMismatch.new(record_class, value.class)
             end
           end
-          
+
           # Override reader for this association if this association belongs
           # to a summary-typed record. This will automatically attempt to download
           # the complete version of the record before accessing the association.
@@ -132,9 +132,9 @@ module Xeroizer
             end
           end
         end
-        
+
       end
-      
+
     end
   end
 end
