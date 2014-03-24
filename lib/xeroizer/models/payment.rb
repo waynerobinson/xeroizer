3faea6829fbc54deac5737535e2de3e50b5ce8b4
Wayne Robinson <wayne.robinson@gmail.com>
2014-03-24 15:49:53 +1100

    Add Linking for CreditNote to Payment

--- lib/xeroizer/models/payment.rb
+++ lib/xeroizer/models/payment.rb
@@ -1,17 +1,17 @@
 module Xeroizer
   module Record
-    
+
     class PaymentModel < BaseModel
-        
+
       set_xml_root_name 'Payments'
       set_permissions :read, :write
-      
+
     end
-    
+
     class Payment < Base
-      
+
       set_primary_key :payment_id
-      
+
       guid          :payment_id
       date          :date
       decimal       :amount
@@ -23,20 +23,25 @@ module Xeroizer
 
       belongs_to    :account
       belongs_to    :invoice
-      
+      belongs_to    :credit_note
+
       def invoice_id
         invoice.id if invoice
       end
-      
+
+      def credit_note_id
+        credit_note.id if credit_note
+      end
+
       def account_id
         account.id if account
       end
-      
+
       def account_code
         account.code if account
       end
-      
+
     end
-    
+
   end
 end
