60c0d8720e713dfb88b5173b103fe42978eb59a1
Alex Ghiculescu <alexghiculescu@gmail.com>
2014-07-14 12:22:17 +1000

    fix home address param names

--- lib/xeroizer/models/payroll/home_address.rb
+++ lib/xeroizer/models/payroll/home_address.rb
@@ -8,10 +8,10 @@ module Xeroizer
       
       class HomeAddress < PayrollBase
         
-        string      :address_line_1
-        string      :address_line_2
-        string      :address_line_3
-        string      :address_line_4
+        string      :address_line1
+        string      :address_line2
+        string      :address_line3
+        string      :address_line4
         string      :city
         string      :region
         string      :postal_code
