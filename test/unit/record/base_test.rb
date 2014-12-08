d29ffa9b5fbbb3d7470faf64ed37408aa1b002a9
David Edwards <david.edwr@gmail.com>
2014-12-08 08:28:10 -0500

    Remove trailing whitespace in record/base_test

--- test/unit/record/base_test.rb
+++ test/unit/record/base_test.rb
@@ -7,32 +7,32 @@ class RecordBaseTest < Test::Unit::TestCase
     @client = Xeroizer::PublicApplication.new(CONSUMER_KEY, CONSUMER_SECRET)
     @contact = @client.Contact.build(:name => 'Test Contact Name ABC')
   end
-  
+
   context "base record" do
-    
+
     should "create new model instance from #new_model_class" do
       model_class = @contact.new_model_class("Invoice")
       assert_kind_of(Xeroizer::Record::InvoiceModel, model_class)
       assert_equal(@contact.parent.application, model_class.application)
       assert_equal('Invoice', model_class.model_name)
     end
-    
+
     should "new_record? should be new on create" do
       assert_equal(true, @contact.new_record?)
     end
-  
+
   end
-  
+
   context "new_record? states" do
-  
+
     should "new_record? should be false when loading data" do
       Xeroizer::OAuth.any_instance.stubs(:get).returns(stub(:plain_body => get_record_xml(:contact), :code => '200'))
       contact = @client.Contact.find('TESTID')
       assert_kind_of(Xeroizer::Record::Contact, contact)
       assert_equal(false, contact.new_record?)
     end
-    
-    should "new_record? should be false after successfully creating a record" do 
+
+    should "new_record? should be false after successfully creating a record" do
       Xeroizer::OAuth.any_instance.stubs(:put).returns(stub(:plain_body => get_record_xml(:contact), :code => '200'))
       assert_equal(true, @contact.new_record?)
       assert_nil(@contact.contact_id)
@@ -40,20 +40,20 @@ class RecordBaseTest < Test::Unit::TestCase
       assert_equal(false, @contact.new_record?)
       assert(@contact.contact_id =~ GUID_REGEX, "@contact.contact_id is not a GUID, it is '#{@contact.contact_id}'")
     end
-    
+
     should "new_record? should be false if we have specified a primary key" do
       contact = @client.Contact.build(:contact_id => 'ABC')
       assert_equal(false, contact.new_record?)
-      
+
       contact = @client.Contact.build(:contact_number => 'CDE')
       assert_equal(true, contact.new_record?)
-      
+
       contact = @client.Contact.build(:name => 'TEST NAME')
       assert_equal(true, contact.new_record?)
     end
-    
+
   end
-  
+
   context "about logging" do
     setup do
       @example_class = Class.new(Xeroizer::Record::Base) do
