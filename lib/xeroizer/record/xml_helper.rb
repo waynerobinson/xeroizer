d62d5f7d59ef5aa08601e3667f5b3e8507e7a847
Alex Ghiculescu <alexghiculescu@gmail.com>
2015-05-04 14:32:05 +1000

    FIX: wasn't getting NumberOfUnit from a timesheeet properly

--- lib/xeroizer/record/xml_helper.rb
+++ lib/xeroizer/record/xml_helper.rb
@@ -68,6 +68,26 @@ module Xeroizer
             end
           end
 
+          # special case for array models eg. NumberOfUnit(s) in http://developer.xero.com/documentation/payroll-api/timesheets/
+          if node.elements.empty? && parent.is_a?(Xeroizer::Record::PayrollArrayBaseModel)
+            field = self.fields[:value]
+            if field
+              element = node.children.first
+              value = case field[:type]
+                when :guid        then element.text
+                when :string      then element.text
+                when :boolean     then (element.text == 'true')
+                when :integer     then element.text.to_i
+                when :decimal     then BigDecimal.new(element.text)
+                when :date        then Date.parse(element.text)
+                when :datetime    then Time.parse(element.text)
+                when :datetime_utc then ActiveSupport::TimeZone['UTC'].parse(element.text).utc
+              end
+
+              record.value = value
+            end
+          end
+
           parent.mark_clean(record)
           record
         end
