56cf77c3b1cb999e5e1e6a5790272907a158bc47
Daniel O'Connor <daniel.oconnor@gmail.com>
2015-05-16 21:31:49 +0930

    For now, remove the modelling for AU/US momentarily

--- lib/xeroizer/models/employee.rb
+++ lib/xeroizer/models/employee.rb
@@ -37,37 +37,37 @@ module Xeroizer
 
     end
 
-    # AU Only API
-    class EmployeeAU < Employee
-      string :occupation
-      string :classification # length: 100
-      string :ordinary_earnings_rate_id
-      boolean :is_authorised_to_approve_leave
+  #   # AU Only API
+  #   class EmployeeAU < Employee
+  #     string :occupation
+  #     string :classification # length: 100
+  #     string :ordinary_earnings_rate_id
+  #     boolean :is_authorised_to_approve_leave
 
-      string :title # (max length = 10)
-      string :twitter_user_name # (max length = 50)
-      # PayrollCalendarID Xero unique identifier for payroll calendar for the employee
-      # BankAccounts  See BankAccount
-      # SuperMemberships  See SuperMemberships
-    end
+  #     string :title # (max length = 10)
+  #     string :twitter_user_name # (max length = 50)
+  #     # PayrollCalendarID Xero unique identifier for payroll calendar for the employee
+  #     # BankAccounts  See BankAccount
+  #     # SuperMemberships  See SuperMemberships
+  #   end
 
-    # US Only API
-    class EmployeeUS < Employee
-      string :middle_names #max length = 35
-      string :job_title
-      string :employee_number
-      string :social_security_number # (xxx-xx-xxxx)
-      string :holiday_group_id
-      string :pay_schedule_id
-      string :employment_basis  # One of http://developer.xero.com/documentation/payroll-api-us/Types-Codes/#EmploymentBasis
-      boolean :is_authorised_to_approve_time_off
+  #   # US Only API
+  #   class EmployeeUS < Employee
+  #     string :middle_names #max length = 35
+  #     string :job_title
+  #     string :employee_number
+  #     string :social_security_number # (xxx-xx-xxxx)
+  #     string :holiday_group_id
+  #     string :pay_schedule_id
+  #     string :employment_basis  # One of http://developer.xero.com/documentation/payroll-api-us/Types-Codes/#EmploymentBasis
+  #     boolean :is_authorised_to_approve_time_off
 
-      # MailingAddress - US only, has_one relationship
-      # SalaryAndWages  See SalaryAndWages
-      # WorkLocations See WorkLocations
-      # PaymentMethod See PaymentMethods
-      # PayTemplate See PayTemplate
-      # OpeningBalances See OpeningBalances
-    end
+  #     # MailingAddress - US only, has_one relationship
+  #     # SalaryAndWages  See SalaryAndWages
+  #     # WorkLocations See WorkLocations
+  #     # PaymentMethod See PaymentMethods
+  #     # PayTemplate See PayTemplate
+  #     # OpeningBalances See OpeningBalances
+  #   end
   end
 end
