69e12296a2c56839107dcec41f15907caedb6b6a
Daniel O'Connor <daniel.oconnor@gmail.com>
2015-07-07 11:59:21 +0930

    Implement fully

--- lib/xeroizer/record/validators/length_of_validator.rb
+++ lib/xeroizer/record/validators/length_of_validator.rb
@@ -6,11 +6,11 @@ module Xeroizer
       class LengthOfValidator < Validator
         
         def valid?(record)
-          if options[:max] && record[attribute].to_s.length > options[:max]
+          if options[:max] && record.attributes[attribute].to_s.length > options[:max]
             record.errors << [attribute, options[:message] || "must be shorter than #{options[:max]} characters"]
           end
 
-          if options[:min] && record[attribute].to_s.length < options[:min]
+          if options[:min] && record.attributes[attribute].to_s.length < options[:min]
             record.errors << [attribute, options[:message] || "must be greater than #{options[:min]} characters"]
           end
         end
--- test/unit/models/phone_test.rb
+++ test/unit/models/phone_test.rb
@@ -5,21 +5,23 @@ class PhoneTest < Test::Unit::TestCase
   
   def setup
     @client = Xeroizer::PublicApplication.new(CONSUMER_KEY, CONSUMER_SECRET)
-    @phone = @client.Phone.build
+    @contact = @client.Contact.build
+
   end
   
   context "validators" do
     
-      validates_length_of :phone, :max => 50
-
     should "not allow long phone numbers" do
-      @phone.phone_number = "1234567890123456789012345678901234567890123456789012345678901234567890"
+      @phone = @contact.add_phone(phone_number: "1234567890123456789012345678901234567890123456789012345678901234567890")
       assert_equal(false, @phone.valid?)
       blank_error = @phone.errors_for(:phone_number).first
       assert_not_nil(blank_error)
-      assert_equal("can't be blank", blank_error)
+      assert_equal("must be shorter than 50 characters", blank_error)
       
-      @phone.phone_number = "1234567890"
+    end
+
+    should "allow phone numbers" do
+      @phone = @contact.add_phone(phone_number: "12345690")
 
       assert_equal(true, @phone.valid?)
       assert_equal(0, @phone.errors.size)
