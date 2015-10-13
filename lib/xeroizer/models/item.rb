fa1b1bb911a1dcece9ee14f5605c91d88c1c8b74
PhilAls <phil.alsford@gmail.com>
2015-10-13 13:08:37 +1300

    Adds Inventory fields to Items

--- lib/xeroizer/models/item.rb
+++ lib/xeroizer/models/item.rb
@@ -15,7 +15,18 @@ module Xeroizer
       guid    :item_id
       string  :code
       string  :description
-      
+      string  :purchase_description
+      string  :name
+
+      decimal :unit_price
+      decimal :total_cost_pool
+      decimal :quantity_on_hand
+
+      boolean :is_sold
+      boolean :is_purchased
+      boolean :is_tracked_as_inventory
+      string  :inventory_asset_account_code
+
       belongs_to :purchase_details, :model_name => 'ItemPurchaseDetails'
       belongs_to :sales_details, :model_name => 'ItemSalesDetails'
       
