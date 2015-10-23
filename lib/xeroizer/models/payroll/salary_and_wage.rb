ce0f3fe03b55a0450ac86d1524f4c61f19aa0e79
Colleen <colleen@objectiveinc.com>
2015-10-23 16:05:47 -0600

    fixing attributes

--- lib/xeroizer/models/payroll/salary_and_wage.rb
+++ lib/xeroizer/models/payroll/salary_and_wage.rb
@@ -14,13 +14,13 @@ module Xeroizer
           'BASEDONORDINARYEARNINGS' => ''
         } unless defined?(SALARY_AND_WAGE_TYPE)
 
-        guid          salary_and_wage_id
-        guid          earnings_type_id
-        string        salary_wages_type
-        decimal       hourly_rate
-        decimal       annual_salary
-        decimal       standard_hours_per_week
-        datetime_utc  effective_date
+        guid          :salary_and_wage_id
+        guid          :earnings_type_id
+        string        :salary_wages_type
+        decimal       :hourly_rate
+        decimal       :annual_salary
+        decimal       :standard_hours_per_week
+        datetime_utc  :effective_date
 
         validates_presence_of :salary_and_wage_id, :unless => :new_record?
         validates_inclusion_of :salary_wages_type, :in => SALARY_AND_WAGE_TYPE
