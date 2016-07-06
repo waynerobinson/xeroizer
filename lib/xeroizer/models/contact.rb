90d4334f4ba7bc7791f2a3c5a915a310c39c2bb0
Jono <jono@platform45.com>
2016-07-06 10:20:57 +0200

    Use summary group attributes on contact to avoid extra API call

--- lib/xeroizer/models/contact.rb
+++ lib/xeroizer/models/contact.rb
@@ -47,7 +47,7 @@ module Xeroizer
 
       has_many  :addresses, :list_complete => true
       has_many  :phones, :list_complete => true
-      has_many  :contact_groups
+      has_many  :contact_groups, :list_complete => true
       has_many  :contact_persons, :internal_name => :contact_people
 
       has_many :sales_tracking_categories, :model_name => 'ContactSalesTrackingCategory'
