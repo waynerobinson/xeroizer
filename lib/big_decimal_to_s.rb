979ed02bf3b77fb629929825e4ce09f83711efeb
Ryan Bigg <git@ryanbigg.com>
2016-07-07 13:48:38 +1000

    Require active_support/big_decimal/conversions instead of custom monkey patch
    This accomplishes the same thing as the monkey patch, but is better code because it is using Module.prepend.

--- lib/big_decimal_to_s.rb
+++
@@ -1,9 +0,0 @@
-class BigDecimal
-  
-  def to_s_with_default_format(format = 'F')
-    to_s_without_default_format(format)
-  end
-  alias_method :to_s_without_default_format, :to_s
-  alias_method :to_s, :to_s_with_default_format
-  
-end
