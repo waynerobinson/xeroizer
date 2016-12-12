205ea63bd2a97c764a0fde9460248296ec3586f5
Daniel O'Connor <daniel.oconnor@gmail.com>
2016-12-13 02:34:05 +1030

    Fix lib/xeroizer/record/base_model.rb:100: warning: instance variable @allow_batch_operations not initialized Fix lib/xeroizer/record/base_model.rb:100: warning: instance variable @objects not initialized

--- lib/xeroizer/record/base_model.rb
+++ lib/xeroizer/record/base_model.rb
@@ -75,6 +75,8 @@ module Xeroizer
         def initialize(application, model_name)
           @application = application
           @model_name = model_name
+          @allow_batch_operations = false
+          @objects = {}
         end
 
         # Retrieve the controller name.
