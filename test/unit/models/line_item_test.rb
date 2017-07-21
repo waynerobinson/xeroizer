3e0248a67decf7e38c3cffbdb65e5a86c5dced52
Chris Green <chrisgreen1993@gmail.com>
2017-07-21 11:07:41 +0100

    include the discount_rate in the line_amount if the LineItem has a discount_rate

--- test/unit/models/line_item_test.rb
+++ test/unit/models/line_item_test.rb
@@ -3,23 +3,23 @@ require "test_helper"
 class LineItemTest < Test::Unit::TestCase
   include TestHelper
   include Xeroizer::Record
-  
+
   def setup
     @client = Xeroizer::PublicApplication.new(CONSUMER_KEY, CONSUMER_SECRET)
   end
-  
+
   it "line_item tracking specified correctly" do
     invoice = @client.Invoice.build
     line_item = invoice.add_line_item({:description => "Test Description", :quantity => 1, :unit_amount => 200})
-    
+
     line_item.add_tracking(:name => "Name 1", :option => "Option 1")
     line_item.add_tracking(:name => "Name 2", :option => "Option 2")
-    
+
     doc = Nokogiri::XML(line_item.to_xml)
     assert_equal 2, doc.xpath("/LineItem/Tracking/TrackingCategory").size
   end
 
-  it "line_amount equals unit_price times quantity" do
+  it "line_amount equals unit_price times quantity if there is no discount_rate" do
     line_item = LineItem.new(nil)
 
     line_item.quantity = 1
@@ -28,7 +28,21 @@ class LineItemTest < Test::Unit::TestCase
 
     expected = BigDecimal((line_item.quantity * (line_item.unit_amount)).to_s).round(2)
 
-    assert_equal expected.to_s, line_item.line_amount.to_s
+    assert_equal expected.to_s, line_item.line_amount.to_s,
+      "expected line_amount to equal unit_amount times quantity"
+  end
+
+  it "line_amount equals unit_amount times quantity minus the discount if there is a discount_rate" do
+    line_item = LineItem.new(nil)
+    line_item.quantity = 1
+    line_item.unit_amount = BigDecimal("1337.00")
+    line_item.tax_amount = BigDecimal("0.15")
+    line_item.discount_rate = BigDecimal("10.0")
+
+    discount = (100 - line_item.discount_rate) / 100
+    expected = BigDecimal(((line_item.quantity * line_item.unit_amount) * discount).to_s).round(2)
+    assert_equal expected.to_s, line_item.line_amount.to_s,
+      "expected line_amount to equal unit_amount times quantity minus the discount"
   end
 
   it "line_amount is zero when quantity is nil or zero" do
