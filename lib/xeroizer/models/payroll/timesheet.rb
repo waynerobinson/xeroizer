206a934602645d8b92cc183954026e3735d36581
Alex Ghiculescu <alex@tanda.co>
2017-10-04 13:50:34 +1000

    UK calls it payroll calendar ID

--- lib/xeroizer/models/payroll/timesheet.rb
+++ lib/xeroizer/models/payroll/timesheet.rb
@@ -1,32 +1,33 @@
 module Xeroizer
   module Record
     module Payroll
-    
+
       class TimesheetModel < PayrollBaseModel
-          
+
         set_permissions :read, :write, :update
-          
+
       end
-      
+
       class Timesheet < PayrollBase
-        
+
         set_primary_key :timesheet_id
 
         guid          :timesheet_id
         guid          :employee_id
+        guid          :payroll_calendar_id # UK
         date          :start_date
         date          :end_date
         decimal       :hours
         string        :status
 
         datetime_utc  :updated_date_utc, :api_name => 'UpdatedDateUTC'
-  
+
         has_many      :timesheet_lines
 
         validates_presence_of :start_date, :end_date, :employee_id
 
       end
 
-    end 
+    end
   end
 end
