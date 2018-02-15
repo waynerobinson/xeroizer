c0d4b6357f0e6773bbecd4b221a8cdb93248d00d
Riley James <rj@rileyjames.co>
2018-02-15 17:10:38 +1100

    rubocop passes

--- lib/xeroizer/models/online_invoice.rb
+++ lib/xeroizer/models/online_invoice.rb
@@ -4,19 +4,19 @@ module Xeroizer
     class OnlineInvoiceModel < BaseModel
 
       module Extensions
-      	def online_invoice(id)
-      		application.OnlineInvoice.online_invoice(url, id)
-      	end
+        def online_invoice(id)
+          application.OnlineInvoice.online_invoice(url, id)
+        end
       end
     
       set_permissions :read
 
       def online_invoice(url, id)
-		    response_xml = @application.http_get(@application.client, "#{url}/#{CGI.escape(id)}/OnlineInvoice")
-		
-	      response = parse_response(response_xml)
+        response_xml = @application.http_get(@application.client, "#{url}/#{CGI.escape(id)}/OnlineInvoice")
+    
+        response = parse_response(response_xml)
 
-		    response.response_items.first
+        response.response_items.first
       end
     
     end
@@ -24,12 +24,12 @@ module Xeroizer
     class OnlineInvoice < Base
 
       module Extensions
-      	def online_invoice
-      	  parent.online_invoice(id)
-      	end
+        def online_invoice
+          parent.online_invoice(id)
+        end
       end
 
-      string   :online_invoice_url
+      string :online_invoice_url
 
     end
     
