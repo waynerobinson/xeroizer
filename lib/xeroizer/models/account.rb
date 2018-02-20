8a5bb3175f267c409a0a29c68d3b0542bcf65707
Riley James <rj@rileyjames.co>
2018-02-20 16:19:15 +1100

    added account archive method

--- lib/xeroizer/models/account.rb
+++ lib/xeroizer/models/account.rb
@@ -3,15 +3,26 @@ module Xeroizer
 
     class AccountModel < BaseModel
 
-      set_permissions :read, :write
+      set_permissions :read, :write, :update
 
-      # The Accounts endpoint doesn't support the POST method yet.
+      # The Accounts endpoint only supports PUT method for creation
+      # Accounts must created one at a time
       def create_method
         :http_put
       end
+
+      # Accounts can be updated using a POST request, only one account can be updated per request
+
     end
 
     class Account < Base
+
+      ACCOUNT_STATUS = {
+        'ACTIVE' => 'Active',
+        'DELETED' => 'Deleted',
+        'ARCHIVED' => 'Archived'
+      } unless defined?(ACCOUNT_STATUS)
+      ACCOUNT_STATUSES = ACCOUNT_STATUS.keys.sort
 
       TYPE = {
         'CURRENT' =>        '',
@@ -65,6 +76,43 @@ module Xeroizer
       string  :reporting_code
       string  :reporting_code_name
       datetime_utc :updated_date_utc, api_name: 'UpdatedDateUTC'
+
+      # validates_inclusion_of :status, :in => ACCOUNT_STATUSES, :unless => :new_record?
+
+      public
+        def archive!
+          archive_account
+        end
+
+      protected
+        def archive_account
+          # account_id = self.id
+          # remove all keys / data
+          clear_account
+
+          # self.id = account_id
+          self.status = "ARCHIVED"
+          self.save
+        end
+
+        # Removes all values from objects, except ID
+        def clear_account
+          self.status = nil
+          self.code = nil
+          self.type = nil
+          self.name = nil
+          self.account_class = nil
+          self.currency_code = nil
+          self.tax_type = nil
+          self.description = nil
+          self.system_account = nil
+          self.enable_payments_to_account = nil
+          self.show_in_expense_claims = nil
+          self.bank_account_number = nil
+          self.reporting_code = nil
+          self.reporting_code_name = nil
+          self.updated_date_utc = nil
+        end
 
     end
 
