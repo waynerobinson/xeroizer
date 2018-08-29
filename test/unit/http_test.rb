7ffdc481f1a4a635da58dedbcd85226b618a3d7a
Nemanja Čorlija <chorlya@gmail.com>
2018-08-29 12:24:26 +0200

    Fix failing tests

--- test/unit/http_test.rb
+++ test/unit/http_test.rb
@@ -3,12 +3,12 @@ require 'test_helper'
 class HttpTest < Test::Unit::TestCase
   include TestHelper
 
-  def setup
-    @headers = {"User-Agent" => "Xeroizer/2.15.5"}
-    @application = Xeroizer::PublicApplication.new(CONSUMER_KEY, CONSUMER_SECRET, :default_headers => @headers)
-  end
-
   context "default_headers" do
+    setup do
+      @headers = { "User-Agent" => "Xeroizer/2.15.5" }
+      @application = Xeroizer::PublicApplication.new(CONSUMER_KEY, CONSUMER_SECRET, :default_headers => @headers)
+    end
+
     should "recognize default_headers" do
       Xeroizer::OAuth.any_instance.expects(:get).with("/test", has_entry(@headers)).returns(stub(:plain_body => "", :code => "200"))
       @application.http_get(@application.client, "http://example.com/test")
