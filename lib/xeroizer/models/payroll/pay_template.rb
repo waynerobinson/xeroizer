a299fe1fcc7bd1a4d78678d05fc086d052969418
Philip Fraser <pr.fraser3@gmail.com>
2019-07-24 12:13:51 +0200

    Pay template url

--- lib/xeroizer/models/payroll/pay_template.rb
+++ lib/xeroizer/models/payroll/pay_template.rb
@@ -1,18 +1,22 @@
 module Xeroizer
   module Record
     module Payroll
-    
+
       class PayTemplateModel < PayrollBaseModel
-          
+
         set_standalone_model true
         set_xml_root_name 'PayTemplate'
         set_xml_node_name 'PayTemplate'
+
+        def api_url(options = {})
+          "employees/#{options.delete(:employee_id)}/paytemplates"
+        end
       end
-      
+
       class PayTemplate < PayrollBase
 
         set_primary_key false
-        
+
         has_many      :earnings_lines
         has_many      :deduction_lines
         has_many      :super_lines
@@ -21,6 +25,6 @@ module Xeroizer
 
       end
 
-    end 
+    end
   end
 end
