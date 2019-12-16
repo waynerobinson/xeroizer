7ed1f3d97b0c6d2df64d3949bb867070c3fe372f
Andy Wendt <awendt@mavenlink.com>
2019-12-16 13:58:16 -0700

    Change bank transfer test to test both clients
    Co-authored-by: Kevin Cotugno <kcotugno@mavenlink.com>

--- test/acceptance/bank_transfer_test.rb
+++ test/acceptance/bank_transfer_test.rb
@@ -4,23 +4,24 @@ require "acceptance_test"
 class BankTransfer < Test::Unit::TestCase
   include AcceptanceTest
 
-  let :client do
-    Xeroizer::PrivateApplication.new(@consumer_key, @consumer_secret, @key_file)
-  end
+  let(:clients) { [oauth_client] }
+  let(:oauth_client) { Xeroizer::PrivateApplication.new(@consumer_key, @consumer_secret, @key_file) }
+  let(:oauth2_client) { Xeroizer::OAuth2Application.new(@client_key, @client_secret, access_token: @access_token, tenant_id: @tenant_id) }
 
-  def setup
-    super
+  def create_a_bank_transfer(client)
     all_accounts = client.Account.all
     @from_bank_account = all_accounts.select { |acct| acct.status == "ACTIVE" && acct.type == "BANK" }.first
     @to_bank_account = all_accounts.select { |acct| acct.status == "ACTIVE" && acct.type == "BANK" }.last
-  end
 
-  can "create a bank transfer" do
     new_transfer = client.BankTransfer.build(
       :amount => 300,
       :from_bank_account => { :account_id => @from_bank_account.account_id },
       :to_bank_account => { :account_id => @to_bank_account.account_id }
     )
     assert new_transfer.save, "Save failed with the following errors: #{new_transfer.errors.inspect}"
+  end
+
+  can "create a bank" do
+    clients.each { |client| create_a_bank_transfer(client) }
   end
 end
