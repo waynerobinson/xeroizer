e3231a4fba7063a14322cc4c56b257a385af013d
Andy Wendt <awendt@mavenlink.com>
2019-12-19 12:10:03 -0700

    Ensure that a failed Xero connection raises an invalid token
    Co-authored-by: Kevin Cotugno <kcotugno@mavenlink.com>
    Co-authored-by: Jordan Higgins <jhiggins@mavenlink.com>

--- test/unit/record/connection_test.rb
+++ test/unit/record/connection_test.rb
@@ -7,10 +7,70 @@ class ConnectionTest < UnitTestCase
     @client = Xeroizer::OAuth2Application.new("client id", "client secret", access_token: "access token")
   end
 
-  it 'returns connections using the passed client' do
-    client = FakeClient.new(get_file_as_string('connections.json'))
-    result = Xeroizer::Connection.current_connections(client)
-    assert_equal 2, result.count
-    assert_equal "7f5fabdd-9565-416a-9b62-d53d852bb2a3", result.first.tenant_id
+  context "when the request succeeds" do
+    setup do
+      stub_request(:get, "https://api.xero.com/connections").
+        with(
+          headers: {
+            'Accept'=>'*/*',
+            'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'Authorization'=>'Bearer access token',
+            'User-Agent'=>'Faraday v0.17.1'
+          }
+        ).to_return(
+        body: [
+          {
+            id: "fe4cd81c-624a-4506-ab18-11c656742436",
+            tenantId: "c84e0419-ade8-4296-9794-289876d4bc22",
+            tenantType: "ORGANISATION",
+            createdDateUtc: "2019-12-13T18:10:43.3063640",
+            updatedDateUtc: "2019-12-13T18:10:43.3084790"
+          },
+          {
+            id: "fe4cd81c-624a-4506-ab18-11c656742436",
+            tenantId: "c84e0419-ade8-4296-9794-289876d4bc22",
+            tenantType: "ORGANISATION",
+            createdDateUtc: "2019-12-13T18:10:43.3063640",
+            updatedDateUtc: "2019-12-13T18:10:43.3084790"
+          }
+        ].to_json
+      )
+    end
+
+    it 'returns connections using the passed client' do
+      result = Xeroizer::Connection.current_connections(@client.client)
+      assert_equal 2, result.count
+      assert_equal "c84e0419-ade8-4296-9794-289876d4bc22", result.first.tenant_id
+    end
+  end
+
+  context 'when the request fails' do
+    setup do
+      @body = {
+        title: "Unauthorized",
+        status: 401,
+        detail: "AuthenticationUnsuccessful",
+        instance: "e1d2cb91-82bf-41a5-84dd-fe6a5c6f070c"
+      }.to_json
+
+      stub_request(:get, "https://api.xero.com/connections").
+        with(
+          headers: {
+            'Accept'=>'*/*',
+            'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'Authorization'=>'Bearer access token',
+            'User-Agent'=>'Faraday v0.17.1'
+          }
+        ).to_return(
+        status: 401,
+        body: @body
+      )
+    end
+
+    it 'raises token_invalid error' do
+      assert_raises(Xeroizer::OAuth::TokenInvalid, @body) {
+        Xeroizer::Connection.current_connections(@client.client)
+      }
+    end
   end
 end
