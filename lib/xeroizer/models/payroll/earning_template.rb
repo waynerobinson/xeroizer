1e96caa6b972fcb5bb2400b2f1fc089fd3a72a2e
Philip Fraser <pr.fraser3@gmail.com>
2020-01-06 13:03:34 +0100

    UPDATE: Update method for earning_template

--- lib/xeroizer/models/payroll/earning_template.rb
+++ lib/xeroizer/models/payroll/earning_template.rb
@@ -30,6 +30,13 @@ module Xeroizer
         "employees/#{employee_id}/leave/paytemplates/earnings/#{pay_template_earning_id}"
       end
 
+      def update
+        params = extra_params_for_create_or_update
+        params[:url] = api_url
+        response = parent.send(:http_put, {}, params)
+        parse_save_response(response)
+      end
+
     end
   end
 end
