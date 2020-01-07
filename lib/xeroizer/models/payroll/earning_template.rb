f9fbb605108e33ba098367a06ce3c1efaf74a1e1
Philip Fraser <pr.fraser3@gmail.com>
2020-01-07 10:41:15 +0100

    UPDATE: Remove update methods from earning_template

--- lib/xeroizer/models/payroll/earning_template.rb
+++ lib/xeroizer/models/payroll/earning_template.rb
@@ -11,13 +11,6 @@ module Xeroizer
           json? ? "employees/#{options.delete(:employee_id)}/paytemplates/earnings/#{options.delete(:pay_template_earning_id)}" : super
         end
 
-        def update
-          params = extra_params_for_create_or_update
-          params[:url] = api_url
-          response = parent.send(:http_put, {}, params)
-          parse_save_response(response)
-        end
-
       end
       # https://developer.xero.com/documentation/payroll-api-uk/employeepaytemplates
       class EarningTemplate < PayrollBase
@@ -35,13 +28,6 @@ module Xeroizer
 
       def api_url
         "employees/#{employee_id}/leave/paytemplates/earnings/#{pay_template_earning_id}"
-      end
-
-      def update
-        params = extra_params_for_create_or_update
-        params[:url] = api_url
-        response = parent.send(:http_put, {}, params)
-        parse_save_response(response)
       end
 
     end
