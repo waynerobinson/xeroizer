7f9ba6a32b7e7cce96ace1f81fc133d5d4241c61
Andy Wendt <awendt@mavenlink.com>
2020-01-27 11:11:09 -0700

    Fix requires/imports for xeroizer
    Co-authored-by: Kevin Cotugno <kcotugno@mavenlink.com>
    Co-authored-by: Jordan Higgins <jhiggins@mavenlink.com>

--- lib/xeroizer.rb
+++ lib/xeroizer.rb
@@ -28,7 +28,7 @@ require 'xeroizer/record/payroll_base_model'
 require 'xeroizer/record/base'
 require 'xeroizer/record/payroll_base'
 require 'xeroizer/configuration'
-require 'xeroizer/response_error_handler'
+require 'xeroizer/oauth_response_error_handler'
 
 # Include models
 require 'xeroizer/models/account'
