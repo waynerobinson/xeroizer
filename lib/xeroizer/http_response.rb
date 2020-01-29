3f7f1739f6a168816a8eb1693530357812fdf032
Andy Wendt <awendt@mavenlink.com>
2020-01-29 15:45:21 -0700

    Extract class from http response as ResponseFailure
    - This is to aid refactoring and is not intended as a final abstraction
    
    Co-authored-by: Jordan Higgins <jhiggins@mavenlink.com>

--- lib/xeroizer/http_response.rb
+++ lib/xeroizer/http_response.rb
@@ -1,10 +1,74 @@
 module Xeroizer
   class BadResponse < XeroizerError; end
 
+  class ResponseFailure
+    def initialize(response, request_body, url)
+      @response = response
+      @request_body = request_body
+      @url = url
+    end
+
+    def raise_error!
+      case response.code.to_i
+      when 400
+        raise_bad_request!
+      when 401
+        AuthFailure.new(response.plain_body).raise_error
+      when 403
+        AuthFailure.new(response.plain_body).raise_error
+      when 404
+        raise_not_found!
+      when 503
+        AuthFailure.new(response.plain_body).raise_error
+      else
+        raise_unknown_response_error!
+      end
+    end
+
+    private
+
+    attr_reader :request_body, :response, :url
+
+    def raise_bad_request!
+
+      raw_response = response.plain_body
+
+      # XeroGenericApplication API Exceptions *claim* to be UTF-16 encoded, but fail REXML/Iconv parsing...
+      # So let's ignore that :)
+      raw_response.gsub! '<?xml version="1.0" encoding="utf-16"?>', ''
+
+      # doc = REXML::Document.new(raw_response, :ignore_whitespace_nodes => :all)
+      doc = Nokogiri::XML(raw_response)
+
+      if doc && doc.root && (doc.root.name == "ApiException" || doc.root.name == 'Response')
+
+        raise ApiException.new(doc.root.xpath("Type").text,
+          doc.root.xpath("Message").text,
+          raw_response,
+          doc,
+          request_body)
+
+      else
+        raise Xeroizer::BadResponse.new("Unparseable 400 Response: #{raw_response}")
+      end
+    end
+
+    def raise_not_found!
+      case url
+      when /Invoices/ then raise InvoiceNotFoundError.new("Invoice not found in Xero.")
+      when /CreditNotes/ then raise CreditNoteNotFoundError.new("Credit Note not found in Xero.")
+      else raise ObjectNotFound.new(url)
+      end
+    end
+
+    def raise_unknown_response_error!
+      raise Xeroizer::BadResponse.new("Unknown response code: #{response.code.to_i}")
+    end
+  end
+
   class AuthFailure
     def initialize(body)
       @body = body
-      @problem = ""
     end
 
     def raise_error
@@ -72,66 +136,13 @@ module Xeroizer
     def body
       response_code = response.code.to_i
       return nil if response_code == 204
-      raise_error! unless response.code.to_i == 200
+      ResponseFailure.new(response, request_body, url).raise_error! unless response.code.to_i == 200
       response.plain_body
     end
 
     private
 
     attr_reader :request_body, :response, :url
-
-    def raise_error!
-      case response.code.to_i
-      when 400
-      raise_bad_request!
-      when 401
-      AuthFailure.new(response.plain_body).raise_error
-      when 403
-      AuthFailure.new(response.plain_body).raise_error
-      when 404
-      raise_not_found!
-      when 503
-      AuthFailure.new(response.plain_body).raise_error
-      else
-        raise_unknown_response_error!
-      end
-    end
-
-    def raise_bad_request!
-
-      raw_response = response.plain_body
-
-      # XeroGenericApplication API Exceptions *claim* to be UTF-16 encoded, but fail REXML/Iconv parsing...
-      # So let's ignore that :)
-      raw_response.gsub! '<?xml version="1.0" encoding="utf-16"?>', ''
-
-      # doc = REXML::Document.new(raw_response, :ignore_whitespace_nodes => :all)
-      doc = Nokogiri::XML(raw_response)
-
-      if doc && doc.root && (doc.root.name == "ApiException" || doc.root.name == 'Response')
-
-        raise ApiException.new(doc.root.xpath("Type").text,
-          doc.root.xpath("Message").text,
-          raw_response,
-          doc,
-          request_body)
-
-      else
-        raise Xeroizer::BadResponse.new("Unparseable 400 Response: #{raw_response}")
-      end
-    end
-
-    def raise_not_found!
-      case url
-      when /Invoices/ then raise InvoiceNotFoundError.new("Invoice not found in Xero.")
-      when /CreditNotes/ then raise CreditNoteNotFoundError.new("Credit Note not found in Xero.")
-      else raise ObjectNotFound.new(url)
-      end
-    end
-
-    def raise_unknown_response_error!
-      raise Xeroizer::BadResponse.new("Unknown response code: #{response.code.to_i}")
-    end
   end
 end
 
