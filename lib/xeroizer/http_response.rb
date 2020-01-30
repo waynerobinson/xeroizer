03607c24d991be10f71095a1ab752deab31b7f39
Andy Wendt <awendt@mavenlink.com>
2020-01-29 17:09:31 -0700

    Extract xml response logic to its own class
    Co-authored-by: Jordan Higgins <jhiggins@mavenlink.com>

--- lib/xeroizer/http_response.rb
+++ lib/xeroizer/http_response.rb
@@ -2,6 +2,8 @@ module Xeroizer
   class BadResponse < XeroizerError; end
 
   class ResponseFailure
+    attr_reader :request_body, :response, :url
+
     def initialize(response, request_body, url)
       @response = response
       @request_body = request_body
@@ -27,26 +29,67 @@ module Xeroizer
           raise Xeroizer::OAuth::UnknownError.new(description)
         end
       rescue JSON::ParserError
-        case response.code.to_i
-        when 400
-          raise_bad_request!
-        when 401
-          AuthFailure.new(response.plain_body).raise_error
-        when 403
-          AuthFailure.new(response.plain_body).raise_error
-        when 404
-          raise_not_found!
-        when 503
-          AuthFailure.new(response.plain_body).raise_error
-        else
-          raise_unknown_response_error!
+        XmlErrorResponse.new(response, request_body, url).raise_error!
+      end
+    end
+  end
+
+  class XmlErrorResponse
+    def initialize(response, request_body, url)
+      @response = response
+      @request_body = request_body
+      @url = url
+    end
+
+    def raise_error!
+      case response.code.to_i
+      when 400
+        raise_bad_request!
+      when 401
+        raise_error
+      when 403
+        raise_error
+      when 404
+        raise_not_found!
+      when 503
+        raise_error
+      else
+        raise_unknown_response_error!
+      end
+    end
+
+    def raise_error
+      description, problem = parse
+
+      # see http://oauth.pbworks.com/ProblemReporting
+      # In addition to token_expired and token_rejected, Xero also returns
+      # 'rate limit exceeded' when more than 60 requests have been made in
+      # a second.
+      if problem
+        case problem
+        when "token_expired"                then raise OAuth::TokenExpired.new(description)
+        when "token_rejected"               then raise OAuth::TokenInvalid.new(description)
+        when "rate limit exceeded"          then raise OAuth::RateLimitExceeded.new(description)
+        when "consumer_key_unknown"         then raise OAuth::ConsumerKeyUnknown.new(description)
+        when "nonce_used"                   then raise OAuth::NonceUsed.new(description)
+        when "organisation offline"         then raise OAuth::OrganisationOffline.new(description)
+        else raise OAuth::UnknownError.new(problem + ':' + description)
         end
+      else
+        raise OAuth::UnknownError.new("Xero API may be down or the way OAuth errors are provided by Xero may have changed.")
       end
     end
 
     private
 
     attr_reader :request_body, :response, :url
+
+    def parse
+      error_details = CGI.parse(response.plain_body)
+      description   = error_details["oauth_problem_advice"].first
+      problem = error_details["oauth_problem"].first
+      [description, problem]
+    end
 
     def raise_bad_request!
 
@@ -82,45 +125,6 @@ module Xeroizer
 
     def raise_unknown_response_error!
       raise Xeroizer::BadResponse.new("Unknown response code: #{response.code.to_i}")
-    end
-  end
-
-  class AuthFailure
-    def initialize(body)
-      @body = body
-    end
-
-    def raise_error
-      description, problem = parse
-
-      # see http://oauth.pbworks.com/ProblemReporting
-      # In addition to token_expired and token_rejected, Xero also returns
-      # 'rate limit exceeded' when more than 60 requests have been made in
-      # a second.
-      if problem
-        case problem
-        when "token_expired"                then raise OAuth::TokenExpired.new(description)
-        when "token_rejected"               then raise OAuth::TokenInvalid.new(description)
-        when "rate limit exceeded"          then raise OAuth::RateLimitExceeded.new(description)
-        when "consumer_key_unknown"         then raise OAuth::ConsumerKeyUnknown.new(description)
-        when "nonce_used"                   then raise OAuth::NonceUsed.new(description)
-        when "organisation offline"         then raise OAuth::OrganisationOffline.new(description)
-        else raise OAuth::UnknownError.new(problem + ':' + description)
-        end
-      else
-        raise OAuth::UnknownError.new("Xero API may be down or the way OAuth errors are provided by Xero may have changed.")
-      end
-    end
-
-    private
-
-    attr_reader :body
-
-    def parse
-      error_details = CGI.parse(body)
-      description   = error_details["oauth_problem_advice"].first
-      problem = error_details["oauth_problem"].first
-      [description, problem]
     end
   end
 
